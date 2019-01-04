e66bbe46833c33b048a6a2b151fe546cf491d776
Camilo Restrepo <camilo.restrepo@logmein.com>
2019-01-04 11:37:01 -0500

    initial commit

---
+++ client/entry.js
@@ -0,0 +1,17 @@
+
+/**
+ * app.js
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+// Needed for redux-saga es6 generator support
+import '@babel/polyfill';
+
+// Import all the third party stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import root app
+import App from '.App';
